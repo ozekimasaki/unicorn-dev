5492db5c44ea492f4b10d763eafd6380ee655049
MasakiOzeki <74758273+ozekimasaki@users.noreply.github.com>
2025-08-30 03:30:06 +0900

    Remove UnicornStudio script from App.tsx
    Removed UnicornStudio script from App component.

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <div data-us-project="scOAnpA06lkStSlizdtB" style="width:1080px; height: 1080px"></div><script type="text/javascript">!function(){if(!window.UnicornStudio){window.UnicornStudio={isInitialized:!1};var i=document.createElement("script");i.src="https://cdn.jsdelivr.net/gh/hiunicornstudio/unicornstudio.js@v1.4.29/dist/unicornStudio.umd.js",i.onload=function(){window.UnicornStudio.isInitialized||(UnicornStudio.init(),window.UnicornStudio.isInitialized=!0)},(document.head || document.body).appendChild(i)}}();</script>
+
       </div>
     </>
   );
