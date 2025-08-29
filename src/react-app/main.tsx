ee2810d08dda81a49a42e3cb35e50611ed38c819
MasakiOzeki <74758273+ozekimasaki@users.noreply.github.com>
2025-08-30 03:30:24 +0900

    Remove App component and add Unicorn Studio script
    Replaced App component with a div and Unicorn Studio script.

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,10 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+        <div data-us-project="scOAnpA06lkStSlizdtB" style="width:1080px; height: 1080px"></div><script type="text/javascript">!function(){if(!window.UnicornStudio){window.UnicornStudio={isInitialized:!1};var i=document.createElement("script");i.src="https://cdn.jsdelivr.net/gh/hiunicornstudio/unicornstudio.js@v1.4.29/dist/unicornStudio.umd.js",i.onload=function(){window.UnicornStudio.isInitialized||(UnicornStudio.init(),window.UnicornStudio.isInitialized=!0)},(document.head || document.body).appendChild(i)}}();</script>
   </StrictMode>,
 );
